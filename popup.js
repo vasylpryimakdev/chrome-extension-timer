fd5f861dfe27e75ccc4e1b5f85e9c27d6cf16b8a
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-07 15:47:55 +0300

    feat: implement persistent timer with Chrome alarms API

--- popup.js
+++ popup.js
@@ -1,21 +1,27 @@
 const timeElement = document.getElementById("time");
-const currentTIme = new Date().toLocaleTimeString();
+const timerElement = document.getElementById("timer");
 const nameElement = document.getElementById("name");
-timeElement.textContent = `The time is: ${currentTIme}`;
 
-chrome.action.setBadgeText(
-  {
-    text: "TIME",
-  },
-  () => {
-    console.log("Finished settin badge text.");
-  },
-);
+function updateTimeElement() {
+  const currentTime = new Date().toLocaleTimeString();
+
+  timeElement.textContent = `The time is: ${currentTime}`;
+
+  chrome.storage.local.get(["timer"], (res) => {
+    const time = res.timer ?? 0;
+
+    timerElement.textContent = `The timer is at: ${time} seconds`;
+  });
+}
+
+updateTimeElement();
+
+setInterval(() => {
+  updateTimeElement();
+}, 1000);
 
 chrome.storage.sync.get(["name"], (res) => {
   const name = res.name ?? "???";
 
   nameElement.textContent = `Your name is ${name}`;
 });
-
-console.log(this);
